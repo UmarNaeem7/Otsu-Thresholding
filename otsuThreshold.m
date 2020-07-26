7e59ffea231951652290cedb93c15eb5f6858d4b
Muhammad Umar Naeem <49314921+UmarNaeem7@users.noreply.github.com>
2020-07-26 15:27:43 +0500

    Add files via upload

---
+++ otsuThreshold.m
@@ -0,0 +1,35 @@
+function [threshold, im] = otsuThreshold(image)
+%Perform Otsu Thresholding method on an image
+%   Detailed explanation goes here
+
+[p,q,r] = size(image);
+
+if r==3
+    image = rgb2gray(image);
+end
+
+n=imhist(image); % Compute the histogram
+N=sum(n); % sum the values of all the histogram values
+max=0; %initialize maximum to zero
+
+p = zeros(256, 0);
+for i=1:256
+    P(i)=n(i)/N; %Computing the probability of each intensity level
+end
+
+for T=2:255      % step through all thresholds from 2 to 255
+    w0=sum(P(1:T)); % Probability of class 1 (separated by threshold)
+    w1=sum(P(T+1:256)); %probability of class2 (separated by threshold)
+    u0=dot([0:T-1],P(1:T))/w0; % class mean u0
+    u1=dot([T:255],P(T+1:256))/w1; % class mean u1
+    sigma=w0*w1*((u1-u0)^2); % compute sigma i.e variance(between class)
+    if sigma>max % compare sigma with maximum 
+        max=sigma; % update the value of max i.e max=sigma
+        threshold=T-1; % desired threshold corresponds to maximum variance of between class
+    end
+end
+
+im = imbinarize(image, threshold/255);
+
+end
+
